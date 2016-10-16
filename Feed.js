c34097e4332fed6057d5f9d5231f2d1a9526c6e6
jboum <jboum@Corporates-MacBook-Air.local>
2016-10-16 04:03:10 -0400

    list view and feed

--- Feed.js
+++ Feed.js
@@ -12,10 +12,13 @@ var {
   Text,
   View,
   StyleSheet,
-  ListView
+  ListView,
+  ActivityIndicator,
+  Image
 } = ReactNative;
 
 var AuthService = require('./AuthService');
+var moment = require('moment');
 
 class Feed extends Component{ //ES6 syntax
     constructor(props){
@@ -27,7 +30,8 @@ class Feed extends Component{ //ES6 syntax
         });
         //init your state when rendering a component
         this.state = {
-            dataSource: ds.cloneWithRows(['A', 'B', 'C']) //adds data
+            dataSource: ds ,//adds data
+            showProgress: true
         };
 
     }
@@ -40,16 +44,19 @@ class Feed extends Component{ //ES6 syntax
         AuthService.getAuthInfo((err,authInfo) => {
             if (err) throw err;
 
-            var url = "https://api.github.com/users/" + authInfo.user.login + '/received_events';
+            var url = "https://api.github.com/users/" + authInfo.user.login + '/events';
 
             fetch(url, { //react native fetch api
-                headers: authInfo.header
+                headers: authInfo.headers
             })
             .then((response) => response.json())
             .then((responseData)=> {
                 var feedItems = responseData.filter((ev) => ev.type == 'PushEvent');
+                var ds = this.state.dataSource.cloneWithRows(feedItems);
+
                 this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(feedItems)
+                    dataSource: ds,
+                    showProgress: false
                 })
             })
         });
@@ -57,23 +64,58 @@ class Feed extends Component{ //ES6 syntax
 
     }
     renderRow(rowData, sectionId, rowId){
-        return <Text> {rowData} </Text>;
+        return (
+            <View style={styles.row}>
+                <Image 
+                    source={{uri: rowData.actor.avatar_url}}
+                    style={styles.img}
+                />
+
+                <View style={{paddingLeft: 20}}>
+                    <Text> {moment(rowData.created_at).fromNow()} </Text>
+                    <Text> {rowData.actor.login} pushed to </Text>
+                    <Text> {rowData.payload.ref.replace('refs/heads/', '')} </Text>
+                    <Text> {rowData.repo.name} </Text>
+                </View> 
+
+            </View>
+        )
     }
 
     render(){
+        if(this.state.showProgress){
+            return (
+                <View style={styles.view}>
+                    <ActivityIndicator size="large" animating={true} />
+                </View>
+            )
+        }
         return (
             <View style={styles.view}> 
                 <ListView dataSource={this.state.dataSource}
                 renderRow = {this.renderRow.bind(this)} />
 
             </View>
-        )
+        );
     }
 }
 const styles = StyleSheet.create({
     view: {
         flex: 1,
-        justifyContent: 'flex-start'
+        justifyContent: 'flex-start',
+        marginTop: 20
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 20,
+        alignItems: 'center',
+        borderBottomWidth: 1
+    },
+    img: {
+        height: 36,
+        width:36,
+        borderRadius: 18
     }
 });
 
